432eebf94a1d529cd14b240967714f541a0c1c94
Paweł Błahut <78206863+SpecialJayy@users.noreply.github.com>
2025-12-06 14:57:36 +0100

    adding clients, started login in customers implemented securiy checks for database regarding adding new customers

--- clientManager.h
+++ clientManager.h
@@ -2,12 +2,16 @@
 #define AUTOSHARE_CLIENTMANAGER_H
 #include <string>
 
+//dodawanie klientów do bazy danych
 bool addClient(std::string login , std::string password);
 
+//usuwanie klientow z bazy danych
 bool removeClient();
 
+//edytowanie danych klienta
 bool editClient();
 
-int login();
+//logowanie do serwisu
+int login(std::string login, std::string password);
 
 #endif //AUTOSHARE_CLIENTMANAGER_H
--- main.cpp
+++ main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 
 int main() {
 
-    createClient();
+    while (true) {
+        cout<< " Prosze podac komende" << endl;
+        string login;
+        string password;
+        cout << "login: " << endl;
+        cin >> login;
+        cout << "password: " << endl;
+        cin >> password;
+        addClient(login, password);
+    }
 
     return 0;
 }
